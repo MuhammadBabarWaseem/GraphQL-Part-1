13845bee974ad88f161bad334c0905dfd6f28ed6
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2024-01-25 00:36:01 +0500

    GraphQL Lift Off Part-I completed

---
+++ client/src/pages/tracks.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Layout } from "../components";
+import { useQuery ,gql } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
+
+interface TrackInterface {
+  id: string;
+  title: string;
+  thumbnail: string;
+  length: number;
+  modulesCount: number;
+  author: {
+    id: string;
+    name: string;
+    photo: string;
+  };
+}
+
+
+const TRACKS = gql`
+query GetTracks {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    length
+    modulesCount
+    author {
+      id
+      name
+      photo
+    }
+  }
+}
+`;
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+const Tracks = () => {
+
+  const {loading,error,data} = useQuery(TRACKS);
+
+  return <Layout grid>
+  <QueryResult loading={loading} error={error} data={data}>
+      {data?.tracksForHome?.map((track: TrackInterface) => (
+        <TrackCard key={track.id} track={track} />
+      ))} 
+  </QueryResult>
+  </Layout>;
+};
+
+export default Tracks;
